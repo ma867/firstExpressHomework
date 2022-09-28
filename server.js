27eb1090bf46b29d94e283bf8e0977069684a61e
ma867 <ma867@njit.edu>
2022-09-28 19:57:21 -0400

    Magic 8 Ball express application created.

--- server.js
+++ server.js
@@ -38,9 +38,19 @@ app.get('/tip/:total/:tipPercentage', (req, res) => {
     let tip = parseInt(req.params.total) * percentage
     res.send(tip.toString())
 })
+//magic 8 ball
+const responses = ["It is certain", "It is decidedly so", "Without a doubt", 
+                    "Yes definitely","You may rely on it", "As I see it yes", 
+                    "Most likely", "Outlook good","Yes", "Signs point to yes", 
+                    "Reply hazy try again", "Ask again later","Better not tell you now", 
+                    "Cannot predict now", "Concentrate and ask again","Don't count on it", 
+                    "My reply is no", "My sources say no","Outlook not so good", "Very doubtful"]
 
-
- 
+ app.get('/magic/:question', (req, res) => {
+    console.log(responses.length)
+    let response = responses[Math.floor(Math.random() * 20)];
+    res.send(`<h1>${response}</h1>`)
+ })
 //End Routes
  
  
